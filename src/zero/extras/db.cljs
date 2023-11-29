8815f154e7088c8bc1d145acf334847058e849e9
Ray Stubbs <ray.stubbs@xledger.com>
2023-11-29 15:44:16 -0700

    Fix DB bugs

--- src/zero/extras/db.cljs
+++ src/zero/extras/db.cljs
@@ -9,21 +9,21 @@
   :ze.db/path
   (fn [rx path & {:keys [transform]}]
     (let [path (vec path)
-          watch-node-path (vec (interpose :sub path))
+          watch-node-path (if (empty? path) [] (into [:sub] (interpose :sub path)))
           rx (if (ifn? transform) (comp rx transform) rx)]
       (rx (get-in @!db path))
       (swap! !db-watches update-in (conj watch-node-path :rxs) (fnil conj #{}) rx)
       (fn db-stream-cleanup []
-        (let [watch-node (update (get-in @!db watch-node-path) :rxs disj rx)]
+        (let [watch-node (update (get-in @!db-watches watch-node-path) :rxs disj rx)]
           (swap! !db-watches assoc-in watch-node-path watch-node)
           (loop [cur-watch-node watch-node
                  cur-watch-path watch-node-path]
             (cond
               (and (seq cur-watch-path) (empty? (:rxs cur-watch-node)) (empty? (:sub cur-watch-node)))
               (let [parent-watch-path (-> cur-watch-path pop pop)
-                    parent-watch-node (-> (get-in @!db parent-watch-path)
-                                        (update :sub dissoc (-> cur-watch-path pop peek)))]
-                (swap! !db-watches parent-watch-path parent-watch-node)
+                    parent-watch-node (-> (get-in @!db-watches parent-watch-path)
+                                        (update :sub dissoc (-> cur-watch-path peek)))]
+                (swap! !db-watches assoc parent-watch-path parent-watch-node)
                 (recur parent-watch-node parent-watch-path))
 
               :else
@@ -55,7 +55,10 @@
                   (fn [[k sub-node]]
                     (collect-all-sub-paths (conj cur-path k) (:sub sub-node)))
                   watch-node)))]
-      (collect-all-sub-paths changed-path (if (empty? changed-path) @!db-watches (get-in @!db-watches (interpose :sub changed-path)))))))
+      (collect-all-sub-paths changed-path
+        (if (empty? changed-path)
+          @!db-watches
+          (get-in @!db-watches (into [:sub] (interpose :sub changed-path))))))))
 
 (defn patch! [patch]
   (let [[new-db affected-paths]
@@ -79,7 +82,7 @@
           patch)]
     (reset! !db new-db)
     (doseq [path affected-paths
-            rx (:rxs (get-in @!db-watches (interpose :sub path)))]
+            rx (:rxs (get-in @!db-watches (if (empty? path) [] (into [:sub] (interpose :sub path)))))]
       (rx (get-in @!db path)))))
 
 (z/reg-effect
