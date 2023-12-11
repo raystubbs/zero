6a24ad126dfa20faa9a72828bd1219516bbbfcf5
Ray Stubbs <ray.stubbs@xledger.com>
2023-12-11 12:16:25 -0700

    Bug fix in prop case conversion

--- src/zero/impl/base.cljs
+++ src/zero/impl/base.cljs
@@ -1,25 +1,25 @@
 (ns zero.impl.base
   (:require
-   [clojure.string :as str]))
+    [clojure.string :as str]))
 
 (defn words [s]
-  (str/split s #"\W+|(?<=[a-z])(?=[A-Z])"))
+  (str/split s #"\s+|(?<=[^_-])[_-]+(?=[^_-])|(?<=[a-z])(?=[A-Z])"))
 
 (defn cammel-case [s]
   (let [[first-word & rest-words] (words s)]
     (str
-     (str/lower-case first-word)
-     (->> rest-words
-          (map
-           (fn [word]
-             (str (str/upper-case (subs word 0 1)) (str/lower-case (subs word 1)))))
-          str/join))))
+      (str/lower-case first-word)
+      (->> rest-words
+        (map
+          (fn [word]
+            (str (str/upper-case (subs word 0 1)) (str/lower-case (subs word 1)))))
+        str/join))))
 
 (defn snake-case [s]
   (->> s
-       words
-       (map str/lower-case)
-       (str/join "-")))
+    words
+    (map str/lower-case)
+    (str/join "-")))
 
 (defn index-of [pred coll]
   (->>
